a30e153caa4ea7ad5f0d42dee51c086d2e60bbd5
Samara Ribeiro <samarafrb@gmail.com>
2023-06-18 16:56:33 -0300

    FEAT: navbar mobile

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,23 +1,35 @@
-let count= 1;
+let count = 1;
 
-document.getElementById ("radio1") .checked = true
+document.getElementById("radio1").checked = true;
 
-setInterval(function() {
- nextImage();
-}, 6500)
-
+setInterval(function () {
+  nextImage();
+}, 6500);
 
 function nextImage() {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
 
-    count++;
-    if (count>4) {
-        count = 1;
-    }
-
-document.getElementById("radio"+count).checked = true;
-
-
-
+  document.getElementById("radio" + count).checked = true;
 }
 
+/* BOTÃO NAVBAR MOBILE */
 
+var botaoNavbar = document.querySelector(".openbtn");
+var navbarMobile = document.querySelector("#mobileNav");
+
+botaoNavbar.addEventListener("click", function () {
+  if (navbarMobile.style.display === "block") {
+    navbarMobile.style.display = "none";
+  } else {
+    navbarMobile.style.display = "block";
+  }
+});
+
+window.onresize = function () {
+  if (window.innerWidth > 985) {
+    navbarMobile.style.display = "none";
+  }
+};
